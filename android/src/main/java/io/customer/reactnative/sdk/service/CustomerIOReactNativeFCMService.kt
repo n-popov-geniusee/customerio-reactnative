71aaf27148d8e387cd9fe7bfbc3d5ab4fad49ca1
Nick Popov <n.popov@geniusee.com>
2023-01-17 18:29:25 +0200

    remove push service

--- android/src/main/java/io/customer/reactnative/sdk/service/CustomerIOReactNativeFCMService.kt
+++ android/src/main/java/io/customer/reactnative/sdk/service/CustomerIOReactNativeFCMService.kt
@@ -5,13 +5,13 @@ import com.google.firebase.messaging.RemoteMessage
 import io.customer.messagingpush.CustomerIOFirebaseMessagingService
 import io.customer.reactnative.sdk.CustomerIOReactNativeInstance
 
-class CustomerIOReactNativeFCMService : FirebaseMessagingService() {
-    override fun onMessageReceived(message: RemoteMessage) {
-        CustomerIOReactNativeInstance.initializeSDKFromContext(context = applicationContext)
-        CustomerIOFirebaseMessagingService.onMessageReceived(message)
-    }
-
-    override fun onNewToken(token: String) {
-        CustomerIOFirebaseMessagingService.onNewToken(token)
-    }
-}
+//class CustomerIOReactNativeFCMService : FirebaseMessagingService() {
+//    override fun onMessageReceived(message: RemoteMessage) {
+//        CustomerIOReactNativeInstance.initializeSDKFromContext(context = applicationContext)
+//        CustomerIOFirebaseMessagingService.onMessageReceived(message)
+//    }
+//
+//    override fun onNewToken(token: String) {
+//        CustomerIOFirebaseMessagingService.onNewToken(token)
+//    }
+//}
